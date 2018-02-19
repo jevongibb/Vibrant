662a31b5f5566adad2da610a39eab63cb03e1e11
optimus.sma <optimus.sma@gmail.com>
2018-02-20 00:01:57 +0200

    v0.17 - Add legend to Local #7

--- js-gulp/load-local.js
+++ js-gulp/load-local.js
@@ -30,9 +30,9 @@
       this.height = this.element.offsetHeight || 400;
       this.margin = {
         top: 30,
-        right: 50,
+        right: 100,
         bottom: 70,
-        left: 50
+        left: 100
       };
       this.width = this.width - (this.margin.left + this.margin.right);
       this.height = this.height - (this.margin.top + this.margin.bottom);
@@ -220,6 +220,57 @@
       build(activeCity);
     });
 
+    addBubbleLegend();
+
+    function addBubbleLegend() {
+      let activityBubbleLegend = d3.select('#local-bubble-legend div');
+      activityBubbleLegend.selectAll('*').remove();
+      // let padding = activityBubbleLegend.node().getBoundingClientRect();
+      let tooltip = activityBubbleLegend
+        .append("div")
+        .attr("class", "local-bubble-legend-tooltip")
+        .style("visibility", "hidden");
+      [{
+        src: 'group1.jpg',
+        tooltip: 'Ag, Fishing, Hunting, & Forestry'
+      }, {
+        src: 'group2.jpg',
+        tooltip: 'Mining, Utilities, & Construction'
+      }, {
+        src: 'group3.jpg',
+        tooltip: 'Manufacturing'
+      }, {
+        src: 'group4.jpg',
+        tooltip: 'Wholesale, Transport, & Warehousing'
+      }, {
+        src: 'group5.jpg',
+        tooltip: 'Services'
+      }, {
+        src: 'group6.jpg',
+        tooltip: 'Education & Healthcare'
+      }, {
+        src: 'group7.jpg',
+        tooltip: 'Arts & Hospitality'
+      }, {
+        src: 'group8.jpg',
+        tooltip: 'Other'
+      }].map((d, i) => {
+        activityBubbleLegend.append('img') //<img src="img/legend2.png" alt="legend"></div>
+          .attr('src', 'img dump/' + d.src)
+          .attr('alt', 'legend')
+          .style("cursor", "pointer")
+          .on("mouseover", () => {
+            tooltip.text(d.tooltip);
+            tooltip.style("visibility", "visible");
+          })
+          .on("mousemove", function () {
+            // console.log(d3.event.pageY, padding);
+            tooltip.style("top", (i * 24 + 5) + "px").style("right", (40) + "px");
+            // tooltip.style("top", ( Math.abs(d3.event.pageY - padding.top)-10)+"px").style("left",( Math.abs(d3.event.pageX - padding.left)+10)+"px");
+          })
+          .on("mouseout", () => tooltip.style("visibility", "hidden"))
+      });
+    }
 
     function buildTable(data) {
       var sortAscending = true;
