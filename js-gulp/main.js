22da39b96df866225a28ea55d24ce8901181942c
optimus.sma <optimus.sma@gmail.com>
2018-03-05 12:16:08 +0200

    v0.41 - Warning message if Internet Explorer or older browser #25

--- js-gulp/main.js
+++ js-gulp/main.js
@@ -56,6 +56,38 @@ $(function () {
     loadPage({}, true);
   });
 
+  /**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+      // IE 10 or older => return version number
+      return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+      // IE 11 => return version number
+      var rv = ua.indexOf('rv:');
+      return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  var edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+     // Edge (IE 12+) => return version number
+     return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  }
+
+  // other browser
+  return false;
+}
+
+
+
   var events = {
     // "graphs": {
     //   page: "graphs",
@@ -221,7 +253,9 @@ $(function () {
 
       // resize(false);
     });
-
+    if (detectIE()){
+      alert("For best viewing experience, please use a modern browser");
+    }
   };
 
 
